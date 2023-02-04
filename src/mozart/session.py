4f8eaf9c1c411411517fe424b6a47661b50f306c
HoneyFox <gpg@honeyfox.uk>
2023-02-04 15:02:47 +0000

    feat: audio session stuff

---
+++ src/mozart/session.py
@@ -0,0 +1,21 @@
+from typing import List
+from gensound import Signal
+
+from mozart.track import Track
+
+
+class Session:
+    def __init__(self):
+        self.audio_session: List[Track] = []
+        pass
+
+    def addTrack(self, track: Track):
+        self.audio_session.append(track)
+
+    def getSignal(self) -> "Signal":
+        return Signal.mix(
+            [track.getSignal() for track in self.audio_session]
+        )  # type: ignore
+
+    def play(self):
+        self.getSignal().play()  # type: ignore
