d9d7ebf55c6b8f8685a0fe302d6bedfade33a06f
Sean Escreet <sean@escreet.co.uk>
2023-02-04 13:59:33 +0000

    Formatted track.py

--- src/mozart/track.py
+++ src/mozart/track.py
@@ -1,8 +1,8 @@
 from .audio_clip import AudioClip
 from .exceptions import AudioClipOverlapException
-from typing import List, Dict, Union
+from typing import Dict, Union
 import dataclasses
-import bisect
+
 
 @dataclasses.dataclass
 class Timing:
@@ -13,17 +13,20 @@ class Timing:
     def end_time(self) -> float:
         return self.start_time + self.duration
 
-    def overlap(self, timing: 'Timing') -> bool:
+    def overlap(self, timing: "Timing") -> bool:
         return not (
-            (timing.start_time <= self.start_time and timing.end_time <= self.start_time) or
-            (timing.start_time >= self.end_time and timing.end_time >= self.end_time)
+            (
+                timing.start_time <= self.start_time
+                and timing.end_time <= self.start_time
+            )
+            or (timing.start_time >= self.end_time and timing.end_time >= self.end_time)
         )
 
     def __str__(self) -> str:
         return f"{self.start_time}---{self.end_time}"
 
-class Track:
 
+class Track:
     def __init__(self):
         self.audio_track: Dict[AudioClip, Timing] = {}
 
@@ -32,15 +35,17 @@ class Track:
             if _timing.overlap(timing):
                 return clip
         return None
-        
-    def addClip(self, audio_clip: AudioClip, start_time: float, duration: float) -> bool:
+
+    def addClip(
+        self, audio_clip: AudioClip, start_time: float, duration: float
+    ) -> bool:
         timing = Timing(start_time, duration)
         if (clash_clip := self._will_clash(timing)) is not None:
             raise AudioClipOverlapException(f"{timing} will clash with {clash_clip}")
         self.audio_track[audio_clip] = Timing(start_time, duration)
 
-    def play() -> 'Track':
+    def play() -> "Track":
         pass
 
-    def pause() -> 'Track':
+    def pause() -> "Track":
         pass
