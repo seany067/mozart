ecce951cc39a57442651284e5513ddda7aefa1fd
Sean Escreet <sean@escreet.co.uk>
2023-02-04 13:58:31 +0000

    Track Exceptions
    * track now correctly throws exception
    * added clip overlap exception
    * added test for exception

--- src/mozart/track.py
+++ src/mozart/track.py
@@ -1,5 +1,6 @@
 from .audio_clip import AudioClip
-from typing import List, Dict
+from .exceptions import AudioClipOverlapException
+from typing import List, Dict, Union
 import dataclasses
 import bisect
 
@@ -18,25 +19,24 @@ class Timing:
             (timing.start_time >= self.end_time and timing.end_time >= self.end_time)
         )
 
+    def __str__(self) -> str:
+        return f"{self.start_time}---{self.end_time}"
+
 class Track:
 
     def __init__(self):
         self.audio_track: Dict[AudioClip, Timing] = {}
-        pass
 
-    def _will_clash(self, timing: Timing) -> bool:
-        keys: List[AudioClip] = sorted(list(self.audio_track.keys()), key=self.audio_track.get)
-        key_i = bisect.bisect_left(keys, timing.start_time, key=lambda x: self.audio_track[x].start_time) - 1
-        clip0 = self.audio_track[keys[key_i]]
-        if clip0.overlap(timing):
-            return True
-        if key_i + 1 < len(keys):
-            clip1 = self.audio_track[key_i + 1]
-            if clip1.overlap(timing):
-                return True
-        return False
+    def _will_clash(self, timing: Timing) -> Union[AudioClip, None]:
+        for clip, _timing in self.audio_track.items():
+            if _timing.overlap(timing):
+                return clip
+        return None
         
-    def addClip(self, audio_clip: AudioClip, start_time: float, duration: float) -> int:
+    def addClip(self, audio_clip: AudioClip, start_time: float, duration: float) -> bool:
+        timing = Timing(start_time, duration)
+        if (clash_clip := self._will_clash(timing)) is not None:
+            raise AudioClipOverlapException(f"{timing} will clash with {clash_clip}")
         self.audio_track[audio_clip] = Timing(start_time, duration)
 
     def play() -> 'Track':
--- tests/test_track.py
+++ tests/test_track.py
@@ -2,6 +2,7 @@ import pytest
 from mozart import Track
 from mozart.track import Timing
 from mozart import AudioClip
+from mozart.exceptions import AudioClipOverlapException
 
 @pytest.fixture()
 def track() -> Track:
@@ -9,10 +10,20 @@ def track() -> Track:
 
 def test_track_timings(track: Track):
     track.addClip(AudioClip(), 1, 3)
-    assert track._will_clash(Timing(1.1, 1.8))
-    assert track._will_clash(Timing(2, 1))
-    assert track._will_clash(Timing(2, 1.1))
-    assert track._will_clash(Timing(1, 1))
-    assert track._will_clash(Timing(0.9, 1))
+    assert track._will_clash(Timing(1.1, 1.8)) is not None
+    assert track._will_clash(Timing(2, 1)) is not None
+    assert track._will_clash(Timing(2, 1.1)) is not None
+    assert track._will_clash(Timing(1, 1)) is not None
+    assert track._will_clash(Timing(0.9, 1.1)) is not None
 
-    assert not track._will_clash(Timing(4, 1))
+    assert track._will_clash(Timing(4, 1)) is None
+
+
+def test_track_add_clip_throws(track: Track):
+    track.addClip(AudioClip(), 1, 3)
+    try:
+        track.addClip(AudioClip(), 2, 1)
+        assert False, "Exception should be thrown as the clips overlap"
+    except AudioClipOverlapException as e:
+        print(e)
+        assert True, "Exception should be thrown as the clips overlap"
