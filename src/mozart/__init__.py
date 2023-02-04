eab533c1d3401d73c94dc8e9f3806882d9ca3190
Joe Rourke <hello@joerourke.co.uk>
2023-02-04 18:30:43 +0000

    feat: implement drum clip type

--- src/mozart/__init__.py
+++ src/mozart/__init__.py
@@ -1,2 +1,3 @@
 from .track import Track
 from .audio_clip import AudioClip
+from .sample_clip import SampleClip
---
+++ src/mozart/drum_clip.py
@@ -0,0 +1,58 @@
+from .audio_clip import AudioClip
+from gensound import WAV, Signal, Silence, Sine, Raw, Audio
+from pathlib import Path
+
+class DrumInstruments:
+    __instruments: dict[str, str]
+
+    def __init__(self, **kwargs: str):
+        self.__instruments = kwargs
+
+    def get_instruments(self):
+        return self.__instruments
+
+
+class DrumPatterns:
+    __patterns: dict[str, list[float]]
+    __tempo: int
+
+    def __init__(self, tempo: int, **kwargs: list[float]):
+        self.__patterns = kwargs
+        self.__tempo = tempo
+
+    def get_tempo(self):
+        return self.__tempo
+
+    def get_pattern(self):
+        return self.__patterns
+
+
+class DrumClip(AudioClip):
+    __instruments: DrumInstruments
+    __patterns: DrumPatterns
+
+    def __init__(self, instruments: DrumInstruments, patterns: DrumPatterns):
+        self.__instruments = instruments
+        self.__patterns = patterns
+        self.__build_internal_representation()
+
+    def play(self):
+        self.__internal.play()
+
+    def __build_internal_representation(self):
+        beat_length = 30e3 / float(self.__patterns.get_tempo())
+        instruments = self.__instruments.get_instruments()
+        patterns = self.__patterns.get_pattern()
+
+        instrument_wav_map = {instrument: WAV(file) for instrument, file in instruments.items()}
+
+        self.__internal = Signal()
+        self.__internal += list(instrument_wav_map.values())[0] * 0
+
+        for instrument, filename in instruments.items():
+            for beat, attack in enumerate(patterns[instrument]):
+                start_time = float(beat * beat_length)
+                if attack:
+                    self.__internal[start_time:] += instrument_wav_map[instrument]
+                    self.__internal = self.__internal.mixdown(sample_rate=44100)
+                    self.__internal = Raw(self.__internal)
