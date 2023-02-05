9d4f9623a511c36bc9fbedcc0105e1cb5110df1c
HoneyFox <gpg@honeyfox.uk>
2023-02-05 05:37:33 +0000

    feat: effects for pitch/time shift

--- src/mozart/pitch_shift.py
+++ src/mozart/pitch_shift.py
@@ -1,4 +1,4 @@
-from gensound import Signal, Audio
+from gensound import Signal, Audio, Transform, Raw
 from librosa.effects import pitch_shift as shift_pitch, time_stretch as stretch_time
 from numpy import vstack
 
@@ -10,10 +10,26 @@ def pitch_shift(self: Signal, shift_by: int) -> Audio:
     return Audio(sample_rate=44100).from_array(vstack((shifted_left, shifted_right)))
 
 
+class PitchShift(Transform):
+    def __init__(self, shift_by: int):
+        self.shift_by = shift_by
+
+    def realise(self, audio):
+        return Raw(pitch_shift(audio, self.shift_by))
+
+
 def pitch_shift_single(self: Signal, shift_by: int) -> Audio:
     mixed_signal = self.mixdown(sample_rate=44100)
     shifted = shift_pitch(mixed_signal.audio[0], 44100, shift_by)
     return Audio(sample_rate=44100).from_array(shifted)
+
+
+class PitchShiftSingle(Transform):
+    def __init__(self, shift_by: int):
+        self.shift_by = shift_by
+
+    def realise(self, audio):
+        return Raw(pitch_shift_single(audio, self.shift_by))
 
 
 def time_stretch(self: Signal, stretch_by: float) -> Audio:
@@ -21,3 +37,11 @@ def time_stretch(self: Signal, stretch_by: float) -> Audio:
     shifted_left = stretch_time(mixed_signal.audio[0], stretch_by)
     shifted_right = shift_pitch(mixed_signal.audio[1], stretch_by)
     return Audio(sample_rate=44100).from_array(vstack((shifted_left, shifted_right)))
+
+
+class TimeStretch(Transform):
+    def __init__(self, shift_by: int):
+        self.shift_by = shift_by
+
+    def realise(self, audio):
+        return Raw(time_stretch(audio, self.shift_by))
