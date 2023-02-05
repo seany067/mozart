cda9d153e02e8e1317ab00281c8ee6b0a47a917f
Sean Escreet <sean@escreet.co.uk>
2023-02-05 11:59:28 +0000

    Added visualiser how-to to readme and setup tools update

--- README.md
+++ README.md
@@ -16,3 +16,5 @@ Each individual sound component of a song is contained within a subclass of `Aud
 Multiple tracks can be played in parallel, much like tracks in a traditional DAW, through use of the `Session` object. A session can be created and tracks added through `add_track(track: Track)`. This will compile down the underlying gensound objects from each track into one `Audio` object to minimise timing problems (however this can become slow when clips contain lots of concatenation of audio components).
 
 See examples like `tests/songs/lofi_beat.py` and `tests/songs/audio_effects.py` to get a greater idea on how the library works.
+
+To use the visualiser, you first need to import `from mozart.wmp.player import start`. `start` is then a function that will take a `Signal` object, and will play and visualise the audio.
--- setup.py
+++ setup.py
@@ -11,15 +11,20 @@ setup(
     author_email='github@honeyfox.uk, joseph.rourke.2019@live.rhul.ac.uk, sean@escreet.co.uk',
     description="Python music framework",
     long_description=readme(),
-    readme="README.md",
     url="https://github.com/seany067/mozart",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.9",
-    install_requires=["gensound==0.5.3", "numpy==1.24.1"],
+    install_requires=[
+        "gensound==0.5.3",
+        "numpy==1.24.1",
+        "PySide6==6.4.2",
+        "librosa==0.9.2",
+        "ffmpeg-python==0.2.0"
+    ],
     package_dir={'': 'src'},
-    packages=find_packages(where="src/mozart", include="*"),
+    packages=find_packages(where="src", include="*"),
     include_package_data=True,
 )
